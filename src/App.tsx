1337e7433975a6c9e27fcd747eeeccf626bdb3c1
misha434 <missymouse434@gmail.com>
2022-09-19 19:56:01 +0900

    fix: error

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState([
-    { id: "", content: "", date: new Date(), important: false },
-  ]);
+  const [data, setData] = useState([]);
   const getAll = () => {
-    // const request = axios.get('http://localhost:3001/api/notes');
+    // const request = axios.get("http://localhost:3001/api/notes");
     const request = axios.get("https://todo-express-api-v0.fly.dev/");
     return request.then((res) => res.data);
   };
@@ -15,7 +13,6 @@ function App() {
     getAll()
       .then((data) => {
         setData(data);
-        console.log(data);
       })
       .catch((error) => console.error(error))
       .finally(() => {});
@@ -25,12 +22,17 @@ function App() {
     <>
       <div>hello world</div>
       <div>List</div>
-      {data.map((data) => (
-        <div key={data.id}>
-          <li>{data.content}</li>
-          <li>{data.date}</li>
-        </div>
-      ))}
+      {data.length ? (
+        data.map((data) => (
+
+          <ul key={data.id}>
+            <li>{data.content}</li>
+            <li>{data.date}</li>
+          </ul>
+        ))
+      ) : (
+        <p>No item</p>
+      )}
     </>
   );
 }
